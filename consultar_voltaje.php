b8f573dd18bf08cfbc16e22d27b7032ca5f15531
Rodri241005 <156733510+Rodri241005@users.noreply.github.com>
2025-05-12 16:02:49 -0600

    Update consultar_voltaje.php

--- consultar_voltaje.php
+++ consultar_voltaje.php
@@ -1,15 +1,13 @@
 <?php
 header("Content-Type: application/json");
 
-
 $host = "db4free.net";
 $user = "rodrigorrg";
 $password = "36197540";
 $database = "ivorydb";
 
 
-
-$conn = new mysqli($host, $usuario, $contrasena, $base_datos);
+$conn = new mysqli($host, $user, $password, $database);
 
 
 if ($conn->connect_error) {
