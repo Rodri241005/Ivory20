fdc0b93b66bfd403531f4fd1d37968f8df755883
Rodri241005 <156733510+Rodri241005@users.noreply.github.com>
2025-05-12 16:04:39 -0600

    Update guardar_resistencia.php

--- guardar_resistencia.php
+++ guardar_resistencia.php
@@ -1,10 +1,13 @@
 <?php
+
 $host = "db4free.net";
-$user = "rodrigorrg"; 
-$password = "36197540"; 
+$user = "rodrigorrg";
+$password = "36197540";
 $database = "ivorydb";
 
-$conn = new mysqli($host, $user, $pass, $db);
+
+$conn = new mysqli($host, $user, $password, $database);
+
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
