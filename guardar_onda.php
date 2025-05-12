1d0ff93e52a3d245ebc55cd79681967edfb36d5e
Rodri241005 <156733510+Rodri241005@users.noreply.github.com>
2025-05-12 16:04:22 -0600

    Update guardar_onda.php

--- guardar_onda.php
+++ guardar_onda.php
@@ -1,10 +1,13 @@
 <?php
+
 $host = "db4free.net";
-$user = "rodrigorrg"; 
-$password = "36197540"; 
+$user = "rodrigorrg";
+$password = "36197540";
 $database = "ivorydb";
 
+
 $conn = new mysqli($host, $user, $password, $database);
+
 
 if ($conn->connect_error) {
     die(json_encode(["success" => false, "message" => "Conexión fallida"]));
